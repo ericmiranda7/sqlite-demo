b4d748d2017d4f2d9a673e002cdb3c6fc182f503
Eric Miranda <eric.miranda.ext@siemens-energy.com>
2024-10-13 23:23:19 +0530

    sol2

--- app/src/main/java/com/example/splitthat/data/DbContract.kt
+++ app/src/main/java/com/example/splitthat/data/DbContract.kt
@@ -1,5 +1,11 @@
 package com.example.splitthat.data
 
+import android.provider.BaseColumns
+
 object DbContract {
-    // todo(): add a contract class for your schema here
+    object ExpensesTable : BaseColumns {
+        const val TABLE_NAME = "expenses"
+        const val COLUMN_NAME_EXPENSE_NAME = "expense_name"
+        const val COLUMN_NAME_COST = "cost"
+    }
 }
