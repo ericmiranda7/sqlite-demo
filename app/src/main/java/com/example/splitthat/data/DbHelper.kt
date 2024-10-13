be620f1d0a77c42c616eb8538e8e8675ec123503
Eric Miranda <eric.miranda.ext@siemens-energy.com>
2024-10-14 00:01:09 +0530

    add sol3

--- app/src/main/java/com/example/splitthat/data/DbHelper.kt
+++ app/src/main/java/com/example/splitthat/data/DbHelper.kt
@@ -4,6 +4,20 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-// todo(): implement this class
-class DbHelper {
+const val CREATE_SQL = "CREATE TABLE expenses (name TEXT, cost REAL)"
+
+class DbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(CREATE_SQL)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        // schema properties
+        const val DATABASE_NAME = "expenses.db"
+        const val DATABASE_VERSION = 1
+    }
 }
